5ac5ce04a9e19dbe0722dbc9ca62384d0e2f7bc8
vorxz <user@email.com>
2022-04-06 01:23:58 +0200

    Pre exec

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
+#include <sstream>
 #include <math.h>
 #include <fftw3.h>
 
@@ -10,6 +11,7 @@
 #include "src/const.h"
 #include "src/ops.h"
 
+#include <boost/filesystem.hpp>
 //----------GSL libraries---------//
 
 // Random number generator
@@ -19,38 +21,62 @@
 //--------------Main--------------//
 int main(int argc, char **argv)
 {
-    if (argc != 9)
+    double Lx; // voir pq lorsque L augmente il faut un tf plus grand
+    double Ly;
+    double dl;
+    double tf;
+    double dt;
+    double corr_len; // 2; // Indirectly defines the correlation length between sites
+    double V0;
+    int tot_frames;
+    if (argc < 9)
     {
-        printf("Please introduce the arguments: x_size y_size dl_size tot_time dt_time corr_len V0 seed\n");
-        return 0;
+        printf("Please introduce the arguments: x_size y_size dl_size tot_time dt_time corr_len V0 seed tot_frames(optional int) name(optional int)\n");
+        Lx = 100; // voir pq lorsque L augmente il faut un tf plus grand
+        Ly = 100;
+        dl = 1;
+        tf = 50;
+        dt = 1;
+        corr_len = 2; // Indirectly defines the correlation length between sites
+        V0 = 0.1;
+        tot_frames = tf / dt;
     }
-    /*--------------Physical space parameters-----------------*/
-    double Lx = atof(argv[1]); // voir pq lorsque L augmente il faut un tf plus grand
-    double Ly = atof(argv[2]);
-    double dl = atof(argv[3]);
-    int Nx = (int)Lx / dl;
-    int Ny = (int)Ly / dl;
+    else
+    {
+        /*--------------Physical space parameters-----------------*/
+        Lx = atof(argv[1]); // voir pq lorsque L augmente il faut un tf plus grand
+        Ly = atof(argv[2]);
+        dl = atof(argv[3]);
+        /*-------------------Time parameters----------------------*/
+        tf = atof(argv[4]);
+        dt = atof(argv[5]);
+        /*-----------------Potential parameters-------------------*/
+        corr_len = atof(argv[6]); // Indirectly defines the correlation length between sites
+        V0 = atof(argv[7]);
+        /*-----------------------Seed-----------------------------*/
+        gsl_rng_default_seed = atoi(argv[8]);
+        /*----------------------Total frames----------------------*/
+        tot_frames = atoi(argv[9]);
+    }
+
+    int Nx = (int)Lx / dl; // Number of sites on the x-axis
+    int Ny = (int)Ly / dl; // Number of sites on the y-axis
     if (Lx <= dl || Ly <= dl)
     {
         printf("x_size and y_size must be greater than dl_size");
         return -1;
     }
-    /*-------------------Time parameters----------------------*/
-    double tf = atof(argv[4]);
-    double dt = atof(argv[5]);
+
     if (tf <= dt)
     {
         printf("tot_time must be greater than dt_time");
         return -1;
     }
-    /*-----------------Potential parameters-------------------*/
-    double corr_len = atof(argv[6]); // 2; // Indirectly defines the correlation length between sites
-    double V0 = atof(argv[7]);       // 0.1;
-    //int seed = atof(argv[8]);
+
     double *V = (double *)malloc(Nx * Ny * sizeof(double));
-    gsl_rng_default_seed = atof(argv[8]);
-    std::cout << Lx << "\t" << Ly << "\t" << dl << "\t" << tf << "\t" << dt << "\t" << corr_len << "\t" << V0 << std::endl;
-    
+
+    // std::cout << Lx << "\t" << Ly << "\t" << dl << "\t" << tf << "\t" << dt << "\t" << corr_len << "\t" << V0 << std::endl;
+
     corr_pot(V, V0, corr_len, Nx, Ny, dl);
     /*------------------Prepare for simulation---------------*/
     setvar(Nx, Ny, dl, dt);
@@ -66,22 +92,35 @@ int main(int argc, char **argv)
     // Corriger et fixer k phys et non pas k num
 
     /* --------------File output parameters-----------------*/
-    std::string filename;
-    std::ofstream file;
-    int fps = 24;
+    std::ostringstream dirName;
+    dirName << Lx << "x" << Ly << "_V0_" << V0 << "_" << corr_len;
+    std::string fileName;
+    if (atof(argv[10]) == 0)
+        fileName = "debug";
+    else
+        fileName = argv[10];
+
+    if (boost::filesystem::exists("data/" + dirName.str()) == false)
+    {
+        bool mkdir = boost::filesystem::create_directories("data/" + dirName.str());
+        if (mkdir == false)
+            return -2;
+    }
+
+    std::ofstream ph_esp_file("data/" + dirName.str() + "/" + fileName + ".dat", std::ios::trunc);
 
     /*---------------------File output---------------------*/
-    std::ofstream ph_esp_file("data/phase_space_ev.dat", std::ios::trunc);
     if (ph_esp_file.is_open())
     {
-        ph_esp_file << Nx << "\t" << Ny << "\t" << dt << "\t" << tf << std::endl;
-
         for (double t = 0; t < tf; t += dt)
         {
-            for (int i = 0; i < Nx; i++)
-                for (int j = 0; j < Ny; j++)
-                    ph_esp_file << sqrt(Psi_p[i + j * Ny][REAL] * Psi_p[i + j * Ny][REAL] + Psi_p[i + j * Ny][IMAG] * Psi_p[i + j * Ny][IMAG]) << "\t";
-            ph_esp_file << std::endl;
+            if ((int)(t / dt) % (int)tf/tot_frames == 0)
+            {
+                for (int i = 0; i < Nx; i++)
+                    for (int j = 0; j < Ny; j++)
+                        ph_esp_file << sqrt(Psi_p[i + j * Ny][REAL] * Psi_p[i + j * Ny][REAL] + Psi_p[i + j * Ny][IMAG] * Psi_p[i + j * Ny][IMAG]) << "\t";
+                ph_esp_file << std::endl;
+            }
 
             Uev_p(Psi_p, Psi, V, pl_position_momentum, pl_momentum_position);
         }
